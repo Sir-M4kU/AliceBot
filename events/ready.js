2080be829d2a12d8c9c37599dd1fa83726d03a3a
m4kuwu <m4kuwu@github.com>
2022-11-14 14:49:06 -0500

    modificado el 'bot' por 'client'

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`¡Cargado!\nSe inicio como ${client.user.tag}`);
+		client.user.setActivity('Escuchando Comandos', { type: ActivityType.Playing });
 	},
 };
