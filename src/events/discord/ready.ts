20c797bd77dd9f280f583e80e91819f34fad0e9f
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-01-25 22:39:03 -0500

    Added activity mode

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -1,9 +1,13 @@
-import { Events, Client } from "discord.js";
+import { Events, Client, ActivityType } from "discord.js";
 
 export default {
 	name: Events.ClientReady,
 	once: true,
 	execute(client: Client<true>) {
 		console.log(`Logged in as ${client.user.tag}`);
+		client.user.setActivity({
+			type: ActivityType.Listening,
+			name: "new interactions...",
+		});
 	},
 };
