e0a0980910ab64f42555ec91611c83a99f851d5d
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-01-25 22:43:24 -0500

    Modified to update activity

--- src/events/distube/play-song.ts
+++ src/events/distube/play-song.ts
@@ -1,5 +1,5 @@
 import { Events, type Queue, type Song } from "distube";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, ActivityType } from "discord.js";
 
 export default {
 	name: Events.PLAY_SONG,
@@ -11,17 +11,34 @@ export default {
 			isLive,
 			formattedDuration = "",
 			thumbnail = "",
+			url,
 		} = song;
+		const { volume } = queue;
 		const embed = new EmbedBuilder()
 			.setColor(0x6986b2)
 			.setTitle("Now playing")
 			.setDescription(name)
 			.setImage(thumbnail)
+			.setURL(url)
 			.addFields(
-				{ name: "Requested by", value: `${user}` },
-				{ name: "Duration", value: isLive ? "LIVE" : formattedDuration },
+				{ name: "Requested by", value: `${user}`, inline: true },
+				{
+					name: "Duration",
+					value: isLive ? "LIVE" : formattedDuration,
+					inline: true,
+				},
+				{
+					name: "Volume",
+					value: volume.toString(),
+					inline: true,
+				},
 			);
 
+		queue.client.user?.setActivity({
+			type: ActivityType.Streaming,
+			name,
+			url,
+		});
 		await queue.textChannel?.send({ embeds: [embed] });
 	},
 };
