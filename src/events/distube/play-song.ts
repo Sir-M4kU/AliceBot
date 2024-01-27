511929a05451f42112e5936a9e0033271396cbdf
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-01-26 20:39:29 -0500

    Modified for handling button interactions

--- src/events/distube/play-song.ts
+++ src/events/distube/play-song.ts
@@ -1,5 +1,19 @@
 import { Events, type Queue, type Song } from "distube";
-import { EmbedBuilder, ActivityType } from "discord.js";
+import {
+	ComponentType,
+	EmbedBuilder,
+	Message,
+	ActionRowBuilder,
+	type ButtonBuilder,
+} from "discord.js";
+import {
+	ID,
+	NEXT_BUTTON,
+	PLAY_BUTTON,
+	PREV_BUTTON,
+	STOP_BUTTON,
+} from "../../utils/components.js";
+import { STOP } from "../../utils/embeds.js";
 
 export default {
 	name: Events.PLAY_SONG,
@@ -13,32 +27,93 @@ export default {
 			thumbnail = "",
 			url,
 		} = song;
-		const { volume } = queue;
-		const embed = new EmbedBuilder()
-			.setColor(0x6986b2)
-			.setTitle("Now playing")
-			.setDescription(name)
-			.setImage(thumbnail)
-			.setURL(url)
-			.addFields(
-				{ name: "Requested by", value: `${user}`, inline: true },
-				{
-					name: "Duration",
-					value: isLive ? "LIVE" : formattedDuration,
-					inline: true,
-				},
-				{
-					name: "Volume",
-					value: volume.toString(),
-					inline: true,
-				},
-			);
+		const embed = () => {
+			const msg = queue.paused ? "Paused" : "Now playing";
 
-		queue.client.user?.setActivity({
-			type: ActivityType.Streaming,
-			name,
-			url,
-		});
-		await queue.textChannel?.send({ embeds: [embed] });
+			return new EmbedBuilder()
+				.setColor(0x6986b2)
+				.setTitle(msg)
+				.setDescription(name)
+				.setImage(thumbnail)
+				.setURL(url)
+				.addFields(
+					{ name: "Requested by", value: `${user}`, inline: true },
+					{
+						name: "Duration",
+						value: isLive ? "LIVE" : formattedDuration,
+						inline: true,
+					},
+					{
+						name: "Volume",
+						value: queue.volume.toString(),
+						inline: true,
+					},
+				);
+		};
+		if (queue.textChannel) {
+			const row = () => {
+				const prev = queue.previousSongs.length > 0;
+				const next = queue.songs.length > 1;
+				return new ActionRowBuilder<ButtonBuilder>().addComponents(
+					PREV_BUTTON.setDisabled(!prev),
+					PLAY_BUTTON,
+					STOP_BUTTON,
+					NEXT_BUTTON.setDisabled(!next),
+				);
+			};
+			const response = await queue.textChannel.send({
+				embeds: [embed()],
+				components: [row()],
+			});
+			const collector = response.createMessageComponentCollector({
+				componentType: ComponentType.Button,
+				time: song.duration * 1000,
+			});
+			let deleted = false;
+
+			collector.on("collect", async (i) => {
+				await i.deferUpdate();
+				let msg: Message<boolean>;
+
+				if (i.customId === ID.Play) {
+					if (queue.paused) {
+						collector.resetTimer({
+							time: (song.duration - Math.floor(queue.currentTime)) * 1000,
+						});
+						queue.resume();
+					} else {
+						collector.resetTimer({ time: 60_000 });
+						queue.pause();
+					}
+					await response.edit({
+						embeds: [embed()],
+						components: [row()],
+					});
+				} else if (i.customId === ID.Stop) {
+					await queue.stop();
+					msg = await i.followUp({ embeds: [STOP] });
+					deleted = true;
+					collector.stop(ID.Stop);
+				} else if (i.customId === ID.Prev) {
+					await queue.previous();
+					collector.stop(ID.Prev);
+				} else if (i.customId === ID.Next) {
+					await queue.skip();
+					collector.stop(ID.Next);
+				}
+				if (deleted) setTimeout(async () => await msg.delete(), 4000);
+			});
+			collector.once("end", async (_, reason) => {
+				if (reason === ID.Next || reason === ID.Prev) {
+					await response.delete();
+					return;
+				}
+				try {
+					await response.delete();
+				} catch (e) {
+					console.log(e);
+				}
+			});
+		}
 	},
 };
