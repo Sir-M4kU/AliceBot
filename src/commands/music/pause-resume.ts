25cf19bf191114113601146583fdaec15d7749bf
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-01-26 20:40:18 -0500

    Added pause-resume command

---
+++ src/commands/music/pause-resume.ts
@@ -0,0 +1,37 @@
+import {
+	SlashCommandBuilder,
+	type ChatInputCommandInteraction,
+	GuildMember,
+} from "discord.js";
+import { type GuildIdResolvable } from "distube";
+import { NO_CHANNEL, NO_QUEUE, PAUSE } from "../../utils/embeds.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("pause-resume")
+		.setDescription("Pause or resume the queue"),
+	async execute(interaction: ChatInputCommandInteraction) {
+		const defer = await interaction.deferReply();
+		const {
+			voice: { channel },
+		} = interaction.member as GuildMember;
+		const queue = interaction.client.distube.getQueue(
+			interaction.guild as GuildIdResolvable,
+		);
+
+		if (!channel) {
+			await defer.edit({ embeds: [NO_CHANNEL] });
+			setTimeout(async () => await defer.delete(), 4000);
+			return;
+		}
+		if (!queue) {
+			await defer.edit({ embeds: [NO_QUEUE] });
+			setTimeout(async () => await defer.delete(), 4000);
+			return;
+		}
+
+		queue.paused ? queue.resume() : queue.pause();
+		await defer.edit({ embeds: [PAUSE] });
+		setTimeout(async () => await defer.delete(), 4000);
+	},
+};
