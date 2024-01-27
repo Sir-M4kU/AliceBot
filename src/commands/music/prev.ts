d8d220d3647d1b11001d25e555b63ac6f025fb69
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-01-26 20:40:28 -0500

    Added prev command

---
+++ src/commands/music/prev.ts
@@ -0,0 +1,36 @@
+import {
+	SlashCommandBuilder,
+	type ChatInputCommandInteraction,
+	GuildMember,
+} from "discord.js";
+import { type GuildIdResolvable } from "distube";
+import { NO_CHANNEL, NO_QUEUE } from "../../utils/embeds.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("prev")
+		.setDescription("Set back to the previous song"),
+	async execute(interaction: ChatInputCommandInteraction) {
+		const defer = await interaction.deferReply();
+		const {
+			voice: { channel },
+		} = interaction.member as GuildMember;
+		const queue = interaction.client.distube.getQueue(
+			interaction.guild as GuildIdResolvable,
+		);
+
+		if (!channel) {
+			await defer.edit({ embeds: [NO_CHANNEL] });
+			setTimeout(async () => await defer.delete(), 4000);
+			return;
+		}
+		if (!queue) {
+			await defer.edit({ embeds: [NO_QUEUE] });
+			setTimeout(async () => await defer.delete(), 4000);
+			return;
+		}
+
+		await queue.previous();
+		await defer.delete();
+	},
+};
