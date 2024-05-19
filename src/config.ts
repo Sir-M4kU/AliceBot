a8a5a9bea6fa41409a962af16088b288572779f1
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-05-19 16:12:37 -0500

    Init new branch

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import { env } from "node:process";
+import "dotenv/config";
+
+const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } = env;
+
+if (!DISCORD_CLIENT_ID || !DISCORD_CLIENT_SECRET) {
+  throw new Error("Missing environment variables");
+}
+
+export { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET };
---
+++ src/discord-client.ts
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.username}!`);
+});
+
+export { client };
