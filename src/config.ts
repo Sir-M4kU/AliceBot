11d9a24a1c52b33fb24938f3e2ca71bdbb9c54b2
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-05-19 16:15:31 -0500

    Added format command + files formatted

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } = env;
 
 if (!DISCORD_CLIENT_ID || !DISCORD_CLIENT_SECRET) {
-  throw new Error("Missing environment variables");
+	throw new Error("Missing environment variables");
 }
 
 export { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET };
--- src/discord-client.ts
+++ src/discord-client.ts
@@ -1,11 +1,11 @@
 import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user?.username}!`);
+	console.log(`Logged in as ${client.user?.username}!`);
 });
 
 export { client };
